dd6bcad814ad98f10d87e8b21df332fff0713abb
jasonz184 <jasonz184@nycstudents.net>
2025-05-23 15:48:56 +0000

    All checks work for classrooms but I need to add a center and lunchroom check

--- Main.java
+++ Main.java
@@ -10,17 +10,52 @@ public class Main
         Scanner s = new Scanner(System.in);
         System.out.println("What room are you coming from?");
         room = s.nextLine();
-        while(!checkVaildInput(room))
+        System.out.println(check(room));
+        while(!(room.length() == 6) && !(room.length() == 5))
         {
-            System.out.println("Enter the room in the following format with no spaces: Floor-Side-Number");
+            System.out.println("Enter the room in the following format with no spaces: FloorSideNumber");
             room = s.nextLine();
+            System.out.println(check(room));
         }
         s.close();
     }
 
-    public static boolean checkVaildInput(String input)
+    public static boolean check(String room)
     {
-        if(!(input.length() == 3) && !(input.length() == 4)) return false;
-        return true;
+        String first = room.substring(0, 1);
+        String second = room.substring(1, 2);
+        String end = room.substring(2);
+        int floor;
+        int num;
+        boolean vaildSide = false;
+        boolean vaildNum = false;
+        if((first.equalsIgnoreCase("b")))
+        {
+            
+            first = "0";
+        }
+        try {
+            floor = Integer.parseInt(first);
+            num = Integer.parseInt(end);
+        } catch (Exception e) {
+            System.out.println("bad");
+            return false;
+        }
+        if(second.equalsIgnoreCase("n"))
+        {
+            vaildSide = true;
+            vaildNum = num >= 1 && num <= 8;
+        }
+        else if(second.equalsIgnoreCase("e") || second.equalsIgnoreCase("w"))
+        {
+            vaildSide = true;
+            vaildNum = num >= 1 && num <= 26;
+        }
+        else if(second.equalsIgnoreCase("s"))
+        {
+            vaildSide = true;
+            vaildNum = num >= 1 && num <= 13;
+        }
+        return floor >= 0 && floor <= 10 && vaildSide && vaildNum;
     }
 }
